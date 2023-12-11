145328be9b77fea52a74495f9afa72e48f8250c9
Yuvraj Gupta <yuvrajgupta036@gmail.com>
2023-12-11 14:39:33 +0530

    Start playing with routes

--- app.js
+++ app.js
@@ -5,16 +5,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next();
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1 style='color: blue'>Product page</h1>");
 });
 
 app.use((req, res, next) => {
-  console.log("In another middleware!");
   res.send("<h1>Hello from express</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000, () => {
+  console.log("Server now running....");
+});
