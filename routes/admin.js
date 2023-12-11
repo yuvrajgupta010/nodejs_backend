deba09e1f77a68dfbb49b4fb8145b244faedc0cc
Yuvraj Gupta <yuvrajgupta036@gmail.com>
2023-12-11 18:38:43 +0530

    Filtering paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   res.send(
-    "<form action='/product' method='POST'>" +
+    "<form action='/admin/add-product' method='POST'>" +
       "<input type='text' name='title' />" +
       "<input type='number' name='count' />" +
       "<input type='checkbox' name='agree' />" +
@@ -13,7 +14,8 @@ router.get("/add-product", (req, res, next) => {
   );
 });
 
-router.post("/product", (req, res, next) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
