8eb14369fb1e9f9a57e79e76177370e6efbced58
Yuvraj Gupta <yuvrajgupta036@gmail.com>
2023-12-11 16:24:25 +0530

    Parse urlencoded data with body-parser

--- app.js
+++ app.js
@@ -2,15 +2,30 @@
 const http = require("http");
 // Third party
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1 style='color: blue'>Product page</h1>");
+  res.send(
+    "<form action='/product' method='POST'>" +
+      "<input type='text' name='title' />" +
+      "<input type='number' name='count' />" +
+      "<input type='checkbox' name='agree' />" +
+      "<button type='submit'>Submit</button>" +
+      "</form>"
+  );
 });
 
-app.use((req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome to our shop</h1>");
 });
 
 app.listen(3000, () => {
