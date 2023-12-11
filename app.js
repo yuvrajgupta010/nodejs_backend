a8fda6c569ff6f6868466e2351f6029b4b167ed7
Yuvraj Gupta <yuvrajgupta036@gmail.com>
2023-12-11 11:45:12 +0530

    Play with middlewares!

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // Global file
 const http = require("http");
+// Third party
+const express = require("express");
 
-// Custom file
-const routes = require("./routes.js");
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
