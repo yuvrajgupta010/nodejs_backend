ebbdb1c9c9f770164fa57854e35d0d90fd60f89a
Yuvraj Gupta <yuvrajgupta036@gmail.com>
2023-12-17 16:05:59 +0530

    Read and Write data with fs

--- models/product.js
+++ models/product.js
@@ -1,4 +1,19 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const rootPath = require("../util/path");
+
+const p = path.join(rootPath, "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  return fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 class Product {
   constructor(title) {
@@ -6,11 +21,14 @@ class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 }
 
